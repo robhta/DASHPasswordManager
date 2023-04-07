78ddba6f8615797cbad12fe00a972bcf5ce15ec5
Philip <philip.ohm@live.de>
2023-04-07 09:56:53 +0200

    Add types to cryptography

--- pwdmanager/src/backend/crypto/encryption.ts
+++ pwdmanager/src/backend/crypto/encryption.ts
@@ -1,9 +1,9 @@
 import * as crypto from 'crypto'
 
 export type EncryptedEntity = {
-    payload: any;
-    authTag: any,
-    iv: any;
+    payload: string;
+    authTag: Buffer,
+    iv: Buffer;
     index: number;
 }
 
@@ -13,7 +13,7 @@ export class Encryption {
     }
 
     //AES-256-CBC encryption of an HD-Wallet hardened Private Key
-    private encryptPrivateKey(privateKey: any, ENC_VALUE: string) {
+    private encryptPrivateKey(privateKey: Buffer, ENC_VALUE: string): string {
         const hmac = crypto.createHash('sha512', privateKey).digest();
         const aesKey = hmac.subarray(0,32);
         const aesInputVector = hmac.subarray(32,48);
@@ -23,24 +23,24 @@ export class Encryption {
     }
 
     //Derive a MasterKey for De/Encryption of Files.
-    private getMasterKey(privateKey: any) {
+    private getMasterKey(privateKey: Buffer): string {
         const ENC_VALUE = '2d650551248d792eabf628f451200d7f51cb63e46aadcbb1038aacb05e8c8aee2d650551248d792eabf628f451200d7f51cb63e46aadcbb1038aacb05e8c8aee';
         const key = this.encryptPrivateKey(privateKey, ENC_VALUE);
         return key;
     }
 
     //Split the MasterKey to Symmetric Key to use it in AES
-    private getFileEncKey(privateKey: any) {
+    private getFileEncKey(privateKey: string): Buffer {
         return Buffer.from(privateKey).subarray(32,64);
     }
 
-    public getKey(privateKey: any) {
+    public getKey(privateKey: Buffer): Buffer {
         const masterKey = this.getMasterKey(privateKey);
         return this.getFileEncKey(masterKey);
     }
 
     //Encrytion of Payload, the Encrytion of the Password-Entry
-    public fileLevelEncryption(privateKey: any, payload: any): EncryptedEntity {
+    public fileLevelEncryption(privateKey: Buffer, payload: string): EncryptedEntity {
         const aesInputVector = crypto.randomBytes(12);
         const aesKey = privateKey;
         const aes = crypto.createCipheriv('aes-256-gcm', aesKey, aesInputVector);
@@ -56,7 +56,7 @@ export class Encryption {
     }
 
     //Decrytion of an File, here we need some more Information, because we use AES-256-GCM
-    public fileLevelDecryption(aesKey: any, payload: any, authTag: any, aesInputVector: any) {
+    public fileLevelDecryption(aesKey: Buffer, payload: string, authTag: Buffer, aesInputVector: Buffer): string {
         const decipher = crypto.createDecipheriv('aes-256-gcm', aesKey, aesInputVector);
         decipher.setAuthTag(authTag);
         return decipher.update(payload, 'hex', 'utf8') + decipher.final('utf8');
--- pwdmanager/src/backend/crypto/keyGeneration.ts
+++ pwdmanager/src/backend/crypto/keyGeneration.ts
@@ -1,7 +1,7 @@
 import * as Dash from 'dash'
 
 export class KeyGenerator {
-    public getHDWalletHardendKey(mnemonicString: string, passphrase: string, index: number) {
+    public getHDWalletHardendKey(mnemonicString: string, passphrase: string, index: number): Buffer {
         const mnemonic = new Dash.Core.Mnemonic(mnemonicString);
         const seed = mnemonic.toHDPrivateKey(passphrase);
 
